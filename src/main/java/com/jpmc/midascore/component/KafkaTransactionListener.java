c3f701e1bd34a22778e8cccd1f118b26bd8dcf45
kiiviicell <kiivii2009@gmail.com>
2026-03-09 21:52:02 +0530

    Update KafkaTransactionListener.java

--- src/main/java/com/jpmc/midascore/component/KafkaTransactionListener.java
+++ src/main/java/com/jpmc/midascore/component/KafkaTransactionListener.java
@@ -1,5 +1,6 @@
 package com.jpmc.midascore.component;
 
+import com.jpmc.midascore.foundation.Transaction;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
@@ -7,8 +8,8 @@ import org.springframework.stereotype.Component;
 public class KafkaTransactionListener {
 
     @KafkaListener(topics = "${general.kafka-topic}")
-    public void receive(String message) {
-        System.out.println(message);
+    public void receive(Transaction transaction) {
+        System.out.println(transaction.getAmount());
     }
 
 }
