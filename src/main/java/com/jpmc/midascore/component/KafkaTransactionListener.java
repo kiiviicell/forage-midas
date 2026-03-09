5f151dbf8213f95a6445ae627d1bc320fa3053cf
kiiviicell <kiivii2009@gmail.com>
2026-03-09 21:40:42 +0530

    Create KafkaTransactionListener.java

---
+++ src/main/java/com/jpmc/midascore/component/KafkaTransactionListener.java
@@ -0,0 +1,15 @@
+package com.jpmc.midascore.component;
+
+import com.jpmc.midascore.foundation.Transaction;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaTransactionListener {
+
+    @KafkaListener(topics = "${general.kafka-topic}")
+    public void receive(Transaction transaction) {
+        System.out.println("Received transaction amount: " + transaction.getAmount());
+    }
+
+}
