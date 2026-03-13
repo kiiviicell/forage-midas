846f1ffdb92b688d41f963c8e47acf7393e45ae1
kiiviicell <kiivii2009@gmail.com>
2026-03-13 22:05:33 +0530

    Update KafkaConfig.java

--- src/main/java/com/jpmc/midascore/component/KafkaConfig.java
+++ src/main/java/com/jpmc/midascore/component/KafkaConfig.java
@@ -1,43 +1,19 @@
-package com.jpmc.midascore.component;
+@Bean
+public ConsumerFactory<String, Transaction> consumerFactory() {
 
-import com.jpmc.midascore.foundation.Transaction;
-import org.apache.kafka.common.serialization.StringDeserializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.support.serializer.JsonDeserializer;
+    JsonDeserializer<Transaction> deserializer =
+            new JsonDeserializer<>(Transaction.class);
+    deserializer.addTrustedPackages("*");
 
-import java.util.HashMap;
-import java.util.Map;
+    Map<String, Object> props = new HashMap<>();
 
-@Configuration
-public class KafkaConfig {
+    props.put("bootstrap.servers", "localhost:9092");
+    props.put("group.id", "midas");
+    props.put("auto.offset.reset", "earliest");
 
-    @Bean
-    public ConsumerFactory<String, Transaction> consumerFactory() {
-        JsonDeserializer<Transaction> deserializer =
-                new JsonDeserializer<>(Transaction.class);
-        deserializer.addTrustedPackages("*");
-
-        Map<String, Object> props = new HashMap<>();
-
-        return new DefaultKafkaConsumerFactory<>(
-                props,
-                new StringDeserializer(),
-                deserializer
-        );
-    }
-
-    @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, Transaction>
-    kafkaListenerContainerFactory() {
-
-        ConcurrentKafkaListenerContainerFactory<String, Transaction> factory =
-                new ConcurrentKafkaListenerContainerFactory<>();
-
-        factory.setConsumerFactory(consumerFactory());
-        return factory;
-    }
+    return new DefaultKafkaConsumerFactory<>(
+            props,
+            new StringDeserializer(),
+            deserializer
+    );
 }
