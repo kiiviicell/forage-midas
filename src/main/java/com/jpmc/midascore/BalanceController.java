dfe8e17dcc7fc2d39837a647a39016eed2203c22
kiiviicell <kiivii2009@gmail.com>
2026-03-15 22:20:00 +0530

    Create BalanceController.java

---
+++ src/main/java/com/jpmc/midascore/BalanceController.java
@@ -0,0 +1,28 @@
+package com.jpmc.midascore.controller;
+
+import com.jpmc.midascore.foundation.Balance;
+import com.jpmc.midascore.entity.User;
+import com.jpmc.midascore.repository.UserRepository;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class BalanceController {
+
+    private final UserRepository userRepository;
+
+    public BalanceController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/balance")
+    public Balance getBalance(@RequestParam String userId) {
+
+        User user = userRepository.findById(userId).orElse(null);
+
+        if (user == null) {
+            return new Balance(0);
+        }
+
+        return new Balance(user.getBalance());
+    }
+}
